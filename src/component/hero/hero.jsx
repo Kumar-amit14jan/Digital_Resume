636897e2ab13e976084d6e9b35b7ac667701fb11
Amit Kumar <51708840+Kumar-amit14jan@users.noreply.github.com>
2025-04-16 01:52:44 +0000

    change experience

--- src/component/hero/hero.jsx
+++ src/component/hero/hero.jsx
@@ -5,7 +5,7 @@ export function Hero() {
         <section className={styles.container}>
             <div className={styles.content}>
                 <h1 className={styles.title}>Hi, I’m Amit Kr.</h1>
-                <p className={styles.description}>I’m a Full-Stack Developer with 2+ years of experience using React, Angular, Node.js, Express, MongoDB, and MariaDB. I build fast, responsive, and secure web apps with clean UI and robust backend architecture.
+                <p className={styles.description}>I’m a Full-Stack Developer with 2.5+ years of experience using React, Angular, Node.js, Express, MongoDB, and MariaDB. I build fast, responsive, and secure web apps with clean UI and robust backend architecture.
                     Let’s connect and build something awesome!</p>
                 <a className={styles.contactBtn} href="mailto:kumar.amit14jan@gmail.com">Contact Me</a>
             </div>
