d67e6dbed97d60916311e8b4a1ffb90ac8c33a6c
Amit Kumar <51708840+Kumar-amit14jan@users.noreply.github.com>
2025-04-23 20:56:43 +0000

    added project google doc

--- src/data/project.js
+++ src/data/project.js
@@ -1,20 +1,21 @@
 import {project} from "../assets/projects"
 export const projects =[
+    
     {
-      "title": "EMPLOYEE REVIEW",
+      "title": "Google Doc Clone",
+      "imageSrc": project,
+      "description": "A collaborative rich text editor inspired by Google Docs built with React and Node.js for real-time collaboration, editing, and auto-saving.",
+      "skills": ["React", "Express", "Node", "CSS"],
+      "demo": "https://google-doc-clone-odnb.onrender.com/",
+      "source": "https://github.com/Kumar-amit14jan/-Google_Doc_Clone"
+    },
+    {
+      "title": "Employee Review",
       "imageSrc": project,
       "description": "Developed a full-stack Employee Review System with role-based access, performance tracking, and Passport.js authentication.",
       "skills": ["Node", "Express", "EJS"],
-      "demo": "https://www.example.com",
-      "source": "https://www.github.com"
-    },
-    {
-      "title": "Project B",
-      "imageSrc": project,
-      "description": "Developed a full-stack Employee Review System with role-based access, performance tracking, and Passport.js authentication.",
-      "skills": ["React", "Express", "Node", "Sass"],
-      "demo": "https://www.example.com",
-      "source": "https://www.github.com"
+      "demo": "https://employeereview.herokuapp.com/",
+      "source": "https://github.com/Kumar-amit14jan/EmployeeReview"
     },
     {
       "title": "Project C",
